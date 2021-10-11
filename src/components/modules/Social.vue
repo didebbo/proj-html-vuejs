cca57f78392297b7548e40c90cf22deb8a6c0865
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-11 17:40:48 +0200

    aggiunta struttura base footer

---
+++ src/components/modules/Social.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="social">
+    <div class="logo">
+      <img src="/images/theme_eduprime_logo.png" alt="EduPrime Logo" />
+    </div>
+    <div class="description">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt itaque
+      consequuntur provident ipsa saepe aperiam molestiae neque aut iste
+      pariatur incidunt quia, officia quas doloribus impedit minus laudantium id
+      autem.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Social",
+};
+</script>
+
+<style scoped lang="scss">
+.social {
+  .logo {
+    img {
+      height: 2em;
+    }
+  }
+}
+</style>
--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,12 +1,59 @@
 <template>
-  <footer>This is the Footer!</footer>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Social />
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
+          itaque nam, assumenda tenetur beatae ea ad quam natus voluptatum
+          consequuntur blanditiis quas enim omnis ex maiores officiis sapiente
+          reprehenderit tempore.
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
+          itaque nam, assumenda tenetur beatae ea ad quam natus voluptatum
+          consequuntur blanditiis quas enim omnis ex maiores officiis sapiente
+          reprehenderit tempore.
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
+          itaque nam, assumenda tenetur beatae ea ad quam natus voluptatum
+          consequuntur blanditiis quas enim omnis ex maiores officiis sapiente
+          reprehenderit tempore.
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
+import Social from "../modules/Social.vue";
+
 export default {
   name: "Footer",
+  components: {
+    Social,
+  },
 };
 </script>
 
 <style scoped lang="scss">
+@import "../../scss/colors";
+
+footer {
+  padding: 5em;
+  background-color: $edu-color-red;
+  .container {
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      > .col {
+        width: (100% / 4);
+        padding: 0 1em;
+      }
+    }
+  }
+}
 </style>
