54651b980dbb50d861c3a88d3867188e987e27f7
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-11 16:35:19 +0200

    aggiunta struttura base hero section

---
+++ src/components/modules/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="hero">
+    <div class="container">
+      <span class="cover"></span>
+      <span class="on-cover">
+        <div class="title">
+          <h1>Lorem Ipsum</h1>
+        </div>
+        <div class="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          corporis earum rem labore sit dolorem quidem hic vel nesciunt.
+        </div>
+        <div class="actions">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+      </span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../scss/colors";
+// @import "../../scss/mixin";
+</style>
---
+++ src/components/sections/Main.vue
@@ -0,0 +1,12 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style>
+</style>
