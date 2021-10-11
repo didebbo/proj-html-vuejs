50cf82a07d45155ff07899114b35b2996ada0f87
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-11 11:52:03 +0200

    aggiunta struttura base delle macro aree

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer>This is the Footer!</footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>This is the header!</header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+</style>
