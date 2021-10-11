8a3efc6b97c7e73c3e445ddcf72fd2e49fc04a43
didebbo <gianluca.napoletano.93@gmail.com>
2021-10-11 13:28:50 +0200

    aggiunto font Montserrat

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,13 @@ export default {
 </script>
 
 <style lang="scss">
-// @import "~bootstrap/scss/bootstrap";
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap");
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+body {
+  font-family: "Montserrat", sans-serif;
 }
 </style>
